f3a843301296552896b638e8db238ebf3245482a
Rémi Jean <remi.jean@outlook.com>
2021-09-28 00:26:48 +0200

    0.9

--- world/world.gd
+++ world/world.gd
@@ -13,7 +13,7 @@ enum {
 }
 
 const BRUSH_MIN := 1
-const BRUSH_MAX := 6
+const BRUSH_MAX := 8
 const PHYSICS_TYPES = [TYPE_SAND, TYPE_WATER, TYPE_GAS, TYPE_MUD]
 const WEIGHT_TYPES = {
 	TYPE_NONE: 0,
@@ -30,26 +30,27 @@ var current_type := TYPE_STONE
 
 func _process(_delta):
 	var position = world_to_map(get_viewport().get_mouse_position())
+	var brush_size_half = floor(brush_size / 2.0)
 	
 	# Add block
 	if Input.is_action_pressed("add"):
 		for x in brush_size:
 			for y in brush_size:
-				set_cell(position.x - (brush_size / 2.0) + x, position.y - (brush_size / 2.0) + y, current_type)
+				set_cell(position.x - brush_size_half + x, position.y - brush_size_half + y, current_type)
 	
 	# Delete block
 	if Input.is_action_pressed("delete"):
 		for x in brush_size:
 			for y in brush_size:
-				set_cell(position.x - (brush_size / 2.0) + x, position.y - (brush_size / 2.0) + y, TYPE_NONE)
+				set_cell(position.x - brush_size_half + x, position.y - brush_size_half + y, TYPE_NONE)
 	
 	# Brush increase
 	if Input.is_action_just_released("brush_increase"):
-		brush_size = int(min(BRUSH_MAX, brush_size + 1))
+		brush_size = int(min(BRUSH_MAX, brush_size * 2))
 	
 	# Brush decrease
 	elif Input.is_action_just_released("brush_decrease"):
-		brush_size = int(max(BRUSH_MIN, brush_size - 1))
+		brush_size = int(max(BRUSH_MIN, brush_size / 2.0))
 	
 	# Temporary type switch
 	if Input.is_action_just_pressed("stone"):
